f96afa1675ecb7591247f6c8b0df591c1cd14676
danielarnost <danielarnost@utexaas.edu>
2016-11-24 17:55:31 -0500

    exhbs package

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ var express = require ('express');
 var app = express()
 
 
-app.get('/', function (req, res){
+app.get('/', function (req, res){ //one route
 	res.send ('The Homepage');
 
 
 });
-console.log(process.env.USER)
+
 
 app.listen(process.env.PORT || PORT, function(){
-	console.log( "24 happens on " + PORT)
+	
 });
